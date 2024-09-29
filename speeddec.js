d840be6bd8ea10c44ed2ce19dc48d0b0034a8a0a
shaline-chepkoech <chepkoechshaline726@gmail.com>
2024-09-29 15:06:38 +0300

    edited speeddec

--- speeddec.js
+++ speeddec.js
@@ -1,20 +1,32 @@
+//creates the function object
 function calculatepoints() {
+    //prompts for input
     let speed = prompt("Input speed");
+    //converts non integer to integer
     speed = parseInt(speed);
+    //specifies speed limit
     const speedlimit = 70;
     let demeritPointsPerLimitexceeded = 1;
+    //tests speed 
     if (speed < speedlimit){
         return 'Ok'
     }   else {
+        //calculates excess speed
         const excessSpeed = speed - speedlimit;
+        //calculates demerit points
         const demeritPoints = Math.floor(excessSpeed / 5) * demeritPointsPerLimitexceeded;
+        //evaluates demerit points for license suspension
         if ( demeritPoints >= 12){
             return 'Suspend license';}
-
-        return demeritPoints;}
-                
-        }
-        const points = calculatePoints();
-        console.log(`Demerit Points for speed violation : ${points}`)
+               
+            // Displays total demerit points
+        return demeritPoints ;}
+    }            
+        
+    
+        //calls function
+        const points = calculatepoints();
+        ////logs message to console
+        console.log(`Demerit Points for speed violation : ${points}`);
       
    
